8d4353ebef694f075f04a069c04d2d274b60d1ba
moodyXmood <moody2mood@gmail.com>
2022-09-18 11:34:47 +0300

    Зарегестрированы модели в админпанели

--- magtrailer/pages/admin.py
+++ magtrailer/pages/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(ProductImages)
+class ProductImagesAdmin(admin.ModelAdmin):
+    list_display = ['product']
